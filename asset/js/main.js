f542e8f74c945089a3efd5120e81827ff6ff1736
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2022-11-16 16:53:58 -0400

    modificaciones

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,20 +1,37 @@
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 20,
+//     dots: false,
+//     navigation: false,
+//     nav: false,
+//     autoplayTimeout: 3000,
+//     autoplay: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 2
+//         },
+//         1000: {
+//             items: 3
+//         }
+//     }
+// })  Para que desaparezcan los puntos y las fechitas del carrousel
+
 $('.owl-carousel').owlCarousel({
     loop: true,
-    margin: 20,
-    dots: false,
-    navigation: false,
-    nav: false,
-    autoplayTimeout: 3000,
-    autoplay: true,
+    margin: 10,
+    nav: true,
     responsive: {
         0: {
             items: 1
         },
         600: {
-            items: 2
+            items: 3
         },
         1000: {
-            items: 3
+            items: 5
         }
     }
 })
